0be493ca140a8a144480bc5429879f2481add583
Fractal Melon <FractalMelon@gmail.com>
2017-11-05 22:29:20 +0300

    created project

---
+++ AVS_LR_2/AVS_LR_2/AVSLR2.cpp
@@ -0,0 +1,14 @@
+// AVSLR2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+
+int main()
+{
+	int a = 12;
+	std::cout << a << std::endl;
+    return 0;
+}
+
